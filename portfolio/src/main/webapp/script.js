12f35d8c11c5a04fae4b9634d8236c3ef1caa994
Sreekruthi Dubagunta <dskruthi@gmail.com>
2022-08-10 16:50:26 +0000

    Update javascript in accordance with servlets.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['I was born in India', 'I can speak 4 languages', 'My favorite color is lavender'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
@@ -26,3 +26,51 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
+async function showName() {
+    const responseFromServer = await fetch('/hello');
+    const stats = await responseFromServer.json();
+    console.log(stats);
+    //const textFromResponse = await responseFromServer.text();
+    
+    const greet = msg[Math.floor(Math.random() * msg.length)];
+    const nameContainer = document.getElementById('name-container');
+    nameContainer.innerText = greet;
+    //nameContainer.innerHTML = textFromResponse;
+  
+}
+
+/** Creates an <li> element containing text. */
+function createListElement(text) {
+  const liElement = document.createElement('li');
+  liElement.innerText = text;
+  return liElement;
+
+  
+function requestTranslation() {
+    const text = document.getElementById('text').value;
+    const languageCode = document.getElementById('language').value;
+
+    const resultContainer = document.getElementById('result');
+    resultContainer.innerText = 'Loading...';
+
+    const params = new URLSearchParams();
+    params.append('text', text);
+    params.append('languageCode', languageCode);
+
+    fetch('/translate', {
+      method: 'POST',
+      body: params
+    }).then(response => response.text())
+    .then((translatedMessage) => {
+      resultContainer.innerText = translatedMessage;
+    });
+  }
+
+      
+  }
+
+
+
+  
+  
