b9d0463f68e6fa277f43e0071c0b0be3f0465f1e
Sreekruthi Dubagunta <dskruthi@gmail.com>
2022-08-10 16:52:57 +0000

    Add json

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -1,6 +1,8 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,7 +14,30 @@ public class HelloWorldServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    //response.setContentType("text/html;");
+    //response.getWriter().println("Hello Sreekruthi!");
+    String[] msg = {"Hi There!", "Hola!", "Bonjour"};
+    
+    // Convert the server stats to JSON
+    String json = convertToJson(msg);
+
+    // Send the JSON as the response
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
+  }
+
+  private String convertToJson(String[] input) {
+    String json = "{";
+    json += "\"message1\": ";
+    json += "\"" + input[0] + "\"";
+    json += ", ";
+    json += "\"message2\": ";
+    json += "\"" + input[1] + "\"";
+    json += ", ";
+    json += "\"message3\": ";
+    json += input[2];
+    json += "}";
+    return json;
+
   }
 }
